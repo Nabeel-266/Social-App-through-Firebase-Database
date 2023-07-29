59a520f8348de99122914ed28509d937bea3738e
Abu Bakr Computers <nabeelmajeed266@gmail.com>
2023-07-30 00:38:56 +0500

    check files changes

--- Profile/profile-firebase.js
+++ Profile/profile-firebase.js
@@ -344,8 +344,6 @@ function editProfilePicture() {
             }
         },
         (error) => {
-            // A full list of error codes is available at
-            // https://firebase.google.com/docs/storage/web/handle-errors
             switch (error.code) {
                 case 'storage/unauthorized':
                     // User doesn't have permission to access the object
